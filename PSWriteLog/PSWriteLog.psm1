570bf789b081603d6b46e0e91c0908e841675516
Raymond Piller <ray@vertigion.com>
2023-01-07 00:40:11 -0600

    Added module files

---
+++ PSWriteLog/PSWriteLog.psm1
@@ -0,0 +1,26 @@
+#region DEVONLY
+<#
+.Synopsis
+This is the main scaffolding the glues all the pieces together.
+#>
+$ps1s = Get-ChildItem -Path "${PSScriptRoot}\*.ps1" -Recurse -ErrorAction SilentlyContinue
+
+foreach ($import in $ps1s) {
+    try {
+        . $import.FullName
+    } catch {
+        Write-Error -Message "Failed to import function: $($import.FullName): $_"
+    }
+}
+# The PSD1 is generated from the build pipeline.
+#endregion
+$psd1 = Import-PowerShellDataFile ([IO.Path]::Combine($PSScriptRoot, 'PSWriteLog.psd1'))
+$moduleMember = @{
+    Cmdlet = $psd1.CmdletsToExport
+    Function = $psd1.FunctionsToExport
+    Alias = $psd1.AliasesToExport
+}
+if ($psd1.VariablesToExport) {
+    $moduleMember.Set_Item('Variable', $psd1.VariablesToExport)
+}
+Export-ModuleMember @moduleMember
