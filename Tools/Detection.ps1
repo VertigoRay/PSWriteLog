84690906d55f61d952cf96ec124716c2782e926b
Raymond Piller <ray@vertigion.com>
2023-01-10 00:53:09 -0600

    Added sample Config Items

---
+++ Tools/Detection.ps1
@@ -0,0 +1,8 @@
+<#
+.DESCRIPTION
+    This script can be used as part of a configuration item to setup computers with PSWriteLog.
+    This is much simpler than the Tools\Remediation.ps1.
+
+    Just see if we have the latest version of PSWriteLog installed.
+#>
+(Get-Module 'PSWriteLog' -ListAvailable).Version -eq (Find-Module 'PSWriteLog' -ErrorAction 'Ignore').Version
